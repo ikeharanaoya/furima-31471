e34836864c2caf6d5a7fab0730526cad17aaf5fa
ikeharanaoya <jacsha21@gmail.com>
2020-11-11 18:21:13 +0900

    エラ〜メッセージ追加、表示修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,25 @@ class User < ApplicationRecord
   FORMAT_ZENKAKU = /\A[ぁ-んァ-ン一-龥]/
   # 全角カタカナ
   FORMAT_KANA = /\A[ァ-ヶー－]+\z/
+  # 英数字混合
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   
+  # パスワード
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   # ニックネーム
   validates :nickname, presence: true
   # 苗字
   validates :last_name, presence: true,
-    format: { with: FORMAT_ZENKAKU, message: "全角のみで入力して下さい"}
+    format: { with: FORMAT_ZENKAKU, message: "Full-width characters"}
   # 名前
   validates :first_name, presence: true,
-    format: { with: FORMAT_ZENKAKU, message: "全角のみで入力して下さい"}
+    format: { with: FORMAT_ZENKAKU, message: "Full-width characters"}
   # 苗字_カナ
   validates :last_name_kana, presence: true,
-    format: { with: FORMAT_KANA, message: "全角カタカナのみで入力して下さい"}
+    format: { with: FORMAT_KANA, message: "Full-width katakana characters"}
   # 名前_カナ
   validates :first_name_kana, presence: true,
-    format: { with: FORMAT_KANA, message: "全角カタカナのみで入力して下さい"}
+    format: { with: FORMAT_KANA, message: "Full-width katakana characters"}
   # 生年月日
   validates :birth_date, presence: true
 end
