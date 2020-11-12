cf2f06e01f930e66698dae0262445f7bae250c3f
ikeharanaoya <jacsha21@gmail.com>
2020-11-12 12:42:25 +0900

    単体テスト_正常系（ユーザー情報）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,28 @@
 FactoryBot.define do
+  # ユーザーに自動生成
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
+    # 日本語の設定
+    Faker::Config.locale = :ja
+
+    # メールアドレス
+    email                 {Faker::Internet.free_email}
+    # パスワード
+    password              {"test123"}
+    # パスワード再入力
     password_confirmation {password}
+    # ニックネーム
+    nickname              {Faker::Games::Pokemon.name }
+    # 苗字
+    last_name             {Faker::Name.last_name}
+    # 名前
+    first_name            {Faker::Name.first_name}
+    # 苗字_カナ
+    last_name_kana        {"タナカ"}
+    # 名前_カナ
+    first_name_kana       {"タロウ"}
+    # 生年月日
+    birth_date            {Faker::Date::birthday}
   end
 end
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ユーザーを生成
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録（ユーザー情報）' do
+    context '新規登録：正常形' do
+      it "全項目が入力ありの場合、登録できる。" do
+        expect(@user).to be_valid
+      end
+      it "メールアドレスが違う場合は登録できる" do
+        # メールアドレスを設定
+        @user.email = "test123@test.com"
+        # DBに保存
+        @user.save
+        # 新規ユーザー生成
+        another_user = FactoryBot.build(:user)
+        # 違うメールアドレスを設定
+        another_user.email = "sample@sample.com"
+        # 登録確認
+        expect(another_user).to be_valid
+      end
+      it "パスワードは、6文字以上（半角英数字混合）で登録できる" do
+        # パスワードに６文字以上を設定
+        @user.password = "pass12"
+        @user.password_confirmation = "pass12"
+        # 登録確認
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録：異常形' do
+      it "" do
+      end
+    end
+  end
 end
