d0f40c9a2aa1d03e8f221a6eeca2a6a4edf7bb77
ikeharanaoya <jacsha21@gmail.com>
2020-11-14 19:29:25 +0900

    モデルのバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  # 必須項目の設定
+  # 必須項目の確認
   with_options presence: true do
     # 出品画像
     validates :image
@@ -16,16 +16,19 @@ class Item < ApplicationRecord
   # 価格（300〜9,999,999の範囲確認）
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
-  # カテゴリー(プルダウン範囲確認)
-  validates :category_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10, message: 'Select' }
-  # 商品の状態(プルダウン範囲確認)
-  validates :sales_status_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 6, message: 'Select' }
-  # 配送料の負担(プルダウン範囲確認)
-  validates :shipping_fee_status_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 2, message: 'Select' }
-  # 発送元の地域(プルダウン範囲確認)
-  validates :prefecture_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 47, message: 'Select' }
-  # 発送までの日数(プルダウン範囲確認)
-  validates :scheduled_delivery_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3, message: 'Select' }
+  # 必須項目の確認
+  with_options numericality: { greater_than_or_equal_to: 1, message: 'Select' } do
+    # カテゴリー(プルダウン範囲確認)
+    validates :category_id, numericality: { less_than_or_equal_to: 10, message: 'Select' }
+    # 商品の状態(プルダウン範囲確認)
+    validates :sales_status_id, numericality: { less_than_or_equal_to: 6, message: 'Select' }
+    # 配送料の負担(プルダウン範囲確認)
+    validates :shipping_fee_status_id, numericality: { less_than_or_equal_to: 2, message: 'Select' }
+    # 発送元の地域(プルダウン範囲確認)
+    validates :prefecture_id, numericality: { less_than_or_equal_to: 47, message: 'Select' }
+    # 発送までの日数(プルダウン範囲確認)
+    validates :scheduled_delivery_id, numericality: { less_than_or_equal_to: 3, message: 'Select' }
+  end
 
   # user テーブル
   belongs_to :user
