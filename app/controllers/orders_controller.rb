581229b02d0c005e0c4637d780ebd8b0acc38137
ikeharanaoya <jacsha21@gmail.com>
2020-11-18 15:34:49 +0900

    ビューの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,13 @@ class OrdersController < ApplicationController
 
   # 商品購入画面
   def index
+    # 送信されたIDから、商品情報を設定する
+    @item = Item.find(params[:item_id])
+    # ItemBuyモデルの新規オブジェクトを生成
+    @item_buy = ItemBuy.new
+  end
+
+  # 商品購入処理
+  def create
   end
 end
