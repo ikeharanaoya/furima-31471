1233142eb18c1e8dcd74a510b4f2cab5fb63ce42
ikeharanaoya <jacsha21@gmail.com>
2020-11-28 18:33:36 +0900

    クリア処理の追加

--- app/javascript/addresses_search.js
+++ app/javascript/addresses_search.js
@@ -14,12 +14,11 @@ window.addEventListener("DOMContentLoaded", ()=>{
   // 商品購入画面の場合
   if(path.includes(path_items) && path.includes(path_orders) && paramsRegex.test(params)) {
     // 流用ボタン
-    const DiversionBtn = document.getElementById("DiversionBtn");
+    const diversionBtn = document.getElementById("diversion-btn");
 
-    DiversionBtn.addEventListener("click", (e) => { 
+    diversionBtn.addEventListener("click", (e) => { 
       // フォーム送信処理をキャンセル
       e.preventDefault();
-      console.log("click");
       // XMLHttpRequestを生成
       const XHR = new XMLHttpRequest();
       // XMLHttpRequestを初期化
@@ -35,9 +34,36 @@ window.addEventListener("DOMContentLoaded", ()=>{
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
           return null
         }
-        
-      console.log("OK");
+        // ユーザー住所情報を取得
+        const user_addresses = XHR.response.post;
+        // フォームの情報を取得
+        const formResult = document.getElementById("charge-form");
+        // FormDataオブジェクトとして生成して、jsで使えるようにする
+        const formData = new FormData(formResult);
+
+        // ユーザーの住所情報を設定
+        document.getElementById("postal-code").value = user_addresses[0].postal_code;
+        document.getElementById("prefecture").value = user_addresses[0].prefecture_id;
+        document.getElementById("city").value = user_addresses[0].city;
+        document.getElementById("addresses").value = user_addresses[0].addresses;
+        document.getElementById("building").value = user_addresses[0].building;
+        document.getElementById("phone-number").value = user_addresses[0].phone_number;
       };
+    });
+
+    // クリアボタン
+    const resetBtn = document.getElementById("reset-btn");
+
+    resetBtn.addEventListener("click", (e) => { 
+      // フォーム送信処理をキャンセル
+      e.preventDefault();
+      // 住所情報の全項目クリア
+      document.getElementById("postal-code").value = "";
+      document.getElementById("prefecture").value = 0;
+      document.getElementById("city").value = "";
+      document.getElementById("addresses").value = "";
+      document.getElementById("building").value = "";
+      document.getElementById("phone-number").value = "";
     });
   }
 });
